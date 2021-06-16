4925b67cbb174c9cc4109c808cc7c10ffcaf697c
shashank <shashank.yelluri@autoninja.com>
2021-06-16 17:55:07 +0530

    chinna changs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Chinna from './Chinna';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Shashank</h2>
+      <h2>Hloooooo</h2>
+      <Chinna />
     </div>
   );
 }
 
 export default App;
+
