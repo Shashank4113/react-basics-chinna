66eb9be2f6f096325347d185c5fe7706b51e3344
shashank <shashank.yelluri@autoninja.com>
2021-06-24 09:17:18 +0530

    Jun 24

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import NavBar from './NavBar';
 import Footer from './Footer';
+import Home from './Home';
 
 function App() {
   return (
     <div>
         <NavBar /><hr />
-        <div>
-            <h2>Welcome to Chinna Technologies</h2>
-            <p>Here we are going to teach you full stack !</p>
-        </div><hr />
+        <Home /><hr />
         <Footer />
     </div>
   );
---
+++ src/Home.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+const Home = () => {
+
+    const [name, setName] = useState('HSource');
+    const [course, setCourse] = useState('Full Stack');    
+    
+    const handleClick = () => {
+        setCourse('Python')
+    }
+    
+    const handleClick2 = () => {
+        setName('Besant');
+    }
+
+    return ( 
+        <div>
+            <h2>Welcome to { name } Technologies</h2>
+            <h4>This is our { name } web page</h4>
+            <p>Here we are going to teach you the course of { course } and we have very good mentors in { name } !</p>
+            <button onMouseLeave={ handleClick }>Click to change the course</button>
+            <button onMouseEnter={ handleClick2 }>Click to change the name</button>
+        </div>
+     );
+}
+ 
+export default Home;
